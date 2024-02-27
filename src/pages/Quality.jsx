9d17c86223a747bcb62d38755cf6c1f0658d7f52
Ozan Ali Kayacı <ozanalikayaci@gmail.com>
2024-02-27 23:10:28 +0300

    quality page

--- src/pages/Quality.jsx
+++ src/pages/Quality.jsx
@@ -1,9 +1,108 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+
+import cover from "../assets/2003.jpg";
+
+import { Link } from "react-router-dom";
+
+import { motion } from "framer-motion";
+import {
+  Button,
+  Dialog,
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Typography,
+  Input,
+  Checkbox,
+} from "@material-tailwind/react";
 
 function Quality() {
+  const [open, setOpen] = useState(true);
+  const [selectedImg, setSelectedImg] = useState(null);
+
+  const handleOpen = (e) => {
+    if (e.target) {
+      setSelectedImg(e.target.currentSrc);
+    }
+
+    setOpen((cur) => !cur);
+  };
   return (
-    <div>Quality</div>
-  )
+    <section className="flex items-center justify-center flex-col my-10">
+      <div className="w-full">
+        <h1 className="text-2xl font-semibold mb-8 text-primary-blue w-full">
+          <motion.div
+            initial={{ opacity: 0, y: -20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ ease: "easeInOut", duration: 1 }}
+            className="w-full text-center"
+          >
+            // <span className="text-black">Kalite Parkurumuz</span>
+          </motion.div>
+        </h1>
+        <div className="h-96 w-full">
+          <img className=" object-cover h-full w-full" src={cover} alt="" />
+        </div>
+      </div>
+      <div class="flex flex-col items-center justify-center h-[30vh] px-4">
+        <h1 class="text-4xl font-bold text-gray-800 mb-4">Yapım Aşamasında</h1>
+        <p class="text-gray-600 text-lg mb-8">
+          Yakın zamanda tekrar kontrol ediniz.
+        </p>
+      </div>
+      <Dialog
+        size="xs"
+        open={open}
+        handler={handleOpen}
+        className="bg-transparent shadow-none m-8"
+      >
+        <Card className="mx-auto w-full max-w-[24rem]">
+          <CardBody className="flex flex-col gap-4">
+            <Typography className="mb-3 font-normal" variant="paragraph">
+              Kalite Parkurumuz henüz yapım aşamasındadır. Yakın zamanda tekrar
+              kontrol ediniz.
+            </Typography>
+            <Typography className="mb-3 font-normal" variant="paragraph">
+              Bu sırada diğer sayfalarımızı ziyaret edebilirsiniz.
+            </Typography>
+          </CardBody>
+          <CardFooter className="pt-0">
+            <Link to="/certificates" className="pb-2">
+              <Button
+                className="normal-case text-md mb-2"
+                variant="gradient"
+                onClick={handleOpen}
+                fullWidth
+              >
+                Anasayfa
+              </Button>
+            </Link>
+            <Link to="/certificates" className="pb-2">
+              <Button
+                className="normal-case text-md mb-2"
+                variant="gradient"
+                onClick={handleOpen}
+                fullWidth
+              >
+                Sertifikalar
+              </Button>
+            </Link>
+            <Link to="/machines">
+              <Button
+                className="normal-case text-md !bg-primary-blue"
+                variant="gradient"
+                onClick={handleOpen}
+                fullWidth
+              >
+                Makine Parkurumuz
+              </Button>
+            </Link>
+          </CardFooter>
+        </Card>
+      </Dialog>
+    </section>
+  );
 }
 
-export default Quality
+export default Quality;
