d137a0bff03e4be41feb3b2af12337f4084c1341
Ozan Ali Kayacı <ozanalikayaci@gmail.com>
2024-02-29 02:41:04 +0300

    effect update

--- src/pages/ourworks/components/WorkCard.jsx
+++ src/pages/ourworks/components/WorkCard.jsx
@@ -27,28 +27,40 @@ function WorkCard(props) {
 
   return (
     <>
-      <motion.section
-        initial={{ x: -100 }}
-        whileInView={{
-          opacity: 1,
-          transition: { duration: 0.7, ease: "easeInOut" },
-          x: 0,
-        }}
-        viewport={{ once: true }}
-        className="h-full md:max-w-screen-xl flex-col flex items-center justify-center w-full"
-      >
+      <section className="h-full md:max-w-screen-xl flex-col flex items-center justify-center w-full">
         <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-4">
           {props.data.map(({ image }, index) => (
-            <div className="hover:bg-blue-gray-100 border-2 rounded-lg transition-all duration-1000" onClick={(e) => handleOpen(e)} key={index}>
+            <motion.div
+              initial={{ x: -50 , opacity: 0 }}
+              whileInView={{
+                x: 0,
+                opacity: 1,
+                transition: {
+                  duration:
+                    index % 4 == 0
+                      ? 1.8
+                      : index % 4 == 1
+                      ? 1.4
+                      : index % 4 == 2
+                      ? 1.1
+                      : 0.9,
+                  ease: "easeInOut",
+                },
+              }}
+              viewport={{ once: true }}
+              className=" border-2 rounded-lg "
+              onClick={(e) => handleOpen(e)}
+              key={index}
+            >
               <img
-                className="h-80 w-full max-w-full rounded-lg object-cover object-center"
+                className=" hover:bg-blue-gray-100 transition-color duration-1000 h-80 w-full max-w-full rounded-lg object-cover object-center"
                 src={image}
                 alt="gallery-photo"
               />
-            </div>
+            </motion.div>
           ))}
         </div>
-      </motion.section>
+      </section>
 
       <Dialog
         className="flex items-center justify-center transition-all duration-500"
